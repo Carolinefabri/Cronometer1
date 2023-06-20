663ca2774ca3e08b287e1c296c05b90d27bcaa94
Caroline Fabri <118923303+Carolinefabri@users.noreply.github.com>
2023-06-20 16:39:28 +0200

    Add files via upload

---
+++ chronometer.js
@@ -0,0 +1,45 @@
+class Chronometer {
+  constructor() {
+  
+    this.currentTime = 0;
+    this.intervalId = null;
+  }
+ 
+  start(printTimeCallback) {
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (printTimeCallback) printTimeCallback();
+    }, 1000);
+  }
+
+  getMinutes() {
+    return Math.floor(this.currentTime / 60);
+  }
+
+  getSeconds() {
+    return this.currentTime % 60;
+  }
+
+  computeTwoDigitNumber(value) {
+    return value < 10 ? '0' + value : String(value);
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+  }
+
+  reset() {
+    this.currentTime = 0;
+  }
+
+  split() {
+    const minutes = this.computeTwoDigitNumber(this.getMinutes());
+    const seconds = this.computeTwoDigitNumber(this.getSeconds());
+    return `${minutes}:${seconds}`;
+  }
+}
+
+
+
+
+
